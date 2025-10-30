7b66c4c3fb7bc4078f466d3a1c92ff3134ed3ef1
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-30 15:32:45 +0700

    menambahkan thickness di bawah horizontal divider

--- app/src/main/java/com/example/pertemuanlima/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuanlima/TeksEdit.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -18,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
@@ -74,7 +77,16 @@ fun FormDataDiri(modifier: Modifier
                 textAlamat = it
             }
         )
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(id = R.dimen.padding_medium),
+                top = dimensionResource(
+                    id = R.dimen.padding_medium
 
+            )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
 
     }
 }
