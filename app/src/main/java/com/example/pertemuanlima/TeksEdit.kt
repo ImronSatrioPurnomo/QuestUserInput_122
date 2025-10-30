23a05f642bf48c6b1d07ef513a45e6241d464065
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-30 15:00:11 +0700

    menambahkan variable textnama, textalamat, textJK, nama, alamat, dan jenis

--- app/src/main/java/com/example/pertemuanlima/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuanlima/TeksEdit.kt
@@ -1,1 +1,19 @@
 package com.example.pertemuanlima
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import java.lang.reflect.Modifier
+
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+    var TextNama by remember { mutableStateOf(value = "")}
+    var TextAlamat by remember { mutableStateOf(value = "") }
+    var TextJK by remember { mutableStateOf(value = "") }
+
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
+}
