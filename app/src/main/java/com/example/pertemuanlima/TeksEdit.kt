eb9cdfb20aa7c8012115c83a04e724a6ba0593ce
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-30 15:10:42 +0700

    membuat column baru di dalam formdatadiri

--- app/src/main/java/com/example/pertemuanlima/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuanlima/TeksEdit.kt
@@ -1,13 +1,19 @@
 package com.example.pertemuanlima
 
 import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
+
+private val Arrangement.top: Arrangement.Vertical
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -22,5 +28,9 @@ fun FormDataDiri(modifier: Modifier
 
     val gender:List<String> = listOf("Laki-laki","Perempuan")
 
+    Column(modifier= Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
 
+    }
 }
